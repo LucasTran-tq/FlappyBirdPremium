a033ff2d52e87f468fa37b2478c3f9d43825dd29
vnmp11 <64945534+vnmp11@users.noreply.github.com>
2020-11-19 12:22:09 +0700

    Add files via upload

---
+++ FlappyBird_Edited/FlappyBird/ver3/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace ver3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ongduoi1 = new System.Windows.Forms.PictureBox();
+            this.ongtren1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.nutplay = new System.Windows.Forms.PictureBox();
+            this.lbldiem = new System.Windows.Forms.Label();
+            this.ongtren2 = new System.Windows.Forms.PictureBox();
+            this.ongduoi2 = new System.Windows.Forms.PictureBox();
+            this.pb_Title = new System.Windows.Forms.PictureBox();
+            this.pb_GameOver = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ongduoi1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ongtren1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nutplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ongtren2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ongduoi2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Title)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_GameOver)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ongduoi1
+            // 
+            this.ongduoi1.BackColor = System.Drawing.Color.Transparent;
+            this.ongduoi1.Image = ((System.Drawing.Image)(resources.GetObject("ongduoi1.Image")));
+            this.ongduoi1.Location = new System.Drawing.Point(149, 244);
+            this.ongduoi1.Margin = new System.Windows.Forms.Padding(2);
+            this.ongduoi1.Name = "ongduoi1";
+            this.ongduoi1.Size = new System.Drawing.Size(52, 250);
+            this.ongduoi1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ongduoi1.TabIndex = 2;
+            this.ongduoi1.TabStop = false;
+            // 
+            // ongtren1
+            // 
+            this.ongtren1.BackColor = System.Drawing.Color.Transparent;
+            this.ongtren1.Image = ((System.Drawing.Image)(resources.GetObject("ongtren1.Image")));
+            this.ongtren1.Location = new System.Drawing.Point(149, -137);
+            this.ongtren1.Margin = new System.Windows.Forms.Padding(2);
+            this.ongtren1.Name = "ongtren1";
+            this.ongtren1.Size = new System.Drawing.Size(52, 250);
+            this.ongtren1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ongtren1.TabIndex = 3;
+            this.ongtren1.TabStop = false;
+            this.ongtren1.Paint += new System.Windows.Forms.PaintEventHandler(this.ongtren1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // nutplay
+            // 
+            this.nutplay.BackColor = System.Drawing.Color.Transparent;
+            this.nutplay.Image = ((System.Drawing.Image)(resources.GetObject("nutplay.Image")));
+            this.nutplay.Location = new System.Drawing.Point(126, 172);
+            this.nutplay.Margin = new System.Windows.Forms.Padding(2);
+            this.nutplay.Name = "nutplay";
+            this.nutplay.Size = new System.Drawing.Size(102, 68);
+            this.nutplay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.nutplay.TabIndex = 7;
+            this.nutplay.TabStop = false;
+            this.nutplay.Click += new System.EventHandler(this.nutplay_Click);
+            // 
+            // lbldiem
+            // 
+            this.lbldiem.BackColor = System.Drawing.Color.Transparent;
+            this.lbldiem.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldiem.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbldiem.Location = new System.Drawing.Point(11, 9);
+            this.lbldiem.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbldiem.Name = "lbldiem";
+            this.lbldiem.Size = new System.Drawing.Size(60, 32);
+            this.lbldiem.TabIndex = 8;
+            this.lbldiem.Text = "0";
+            this.lbldiem.Click += new System.EventHandler(this.lbldiem_Click);
+            // 
+            // ongtren2
+            // 
+            this.ongtren2.BackColor = System.Drawing.Color.Transparent;
+            this.ongtren2.Image = ((System.Drawing.Image)(resources.GetObject("ongtren2.Image")));
+            this.ongtren2.Location = new System.Drawing.Point(300, -137);
+            this.ongtren2.Margin = new System.Windows.Forms.Padding(2);
+            this.ongtren2.Name = "ongtren2";
+            this.ongtren2.Size = new System.Drawing.Size(52, 250);
+            this.ongtren2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ongtren2.TabIndex = 9;
+            this.ongtren2.TabStop = false;
+            // 
+            // ongduoi2
+            // 
+            this.ongduoi2.BackColor = System.Drawing.Color.Transparent;
+            this.ongduoi2.Image = ((System.Drawing.Image)(resources.GetObject("ongduoi2.Image")));
+            this.ongduoi2.Location = new System.Drawing.Point(300, 244);
+            this.ongduoi2.Margin = new System.Windows.Forms.Padding(2);
+            this.ongduoi2.Name = "ongduoi2";
+            this.ongduoi2.Size = new System.Drawing.Size(52, 250);
+            this.ongduoi2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ongduoi2.TabIndex = 10;
+            this.ongduoi2.TabStop = false;
+            // 
+            // pb_Title
+            // 
+            this.pb_Title.BackColor = System.Drawing.Color.Transparent;
+            this.pb_Title.Image = ((System.Drawing.Image)(resources.GetObject("pb_Title.Image")));
+            this.pb_Title.Location = new System.Drawing.Point(54, 83);
+            this.pb_Title.Name = "pb_Title";
+            this.pb_Title.Size = new System.Drawing.Size(266, 72);
+            this.pb_Title.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_Title.TabIndex = 11;
+            this.pb_Title.TabStop = false;
+            // 
+            // pb_GameOver
+            // 
+            this.pb_GameOver.BackColor = System.Drawing.Color.Transparent;
+            this.pb_GameOver.Image = ((System.Drawing.Image)(resources.GetObject("pb_GameOver.Image")));
+            this.pb_GameOver.Location = new System.Drawing.Point(49, 99);
+            this.pb_GameOver.Name = "pb_GameOver";
+            this.pb_GameOver.Size = new System.Drawing.Size(271, 68);
+            this.pb_GameOver.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_GameOver.TabIndex = 11;
+            this.pb_GameOver.TabStop = false;
+            this.pb_GameOver.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(364, 416);
+            this.Controls.Add(this.lbldiem);
+            this.Controls.Add(this.pb_GameOver);
+            this.Controls.Add(this.pb_Title);
+            this.Controls.Add(this.ongduoi2);
+            this.Controls.Add(this.ongtren2);
+            this.Controls.Add(this.ongtren1);
+            this.Controls.Add(this.ongduoi1);
+            this.Controls.Add(this.nutplay);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(380, 455);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Flappy Bird";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.ongduoi1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ongtren1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nutplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ongtren2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ongduoi2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Title)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_GameOver)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ongduoi1;
+        private System.Windows.Forms.PictureBox ongtren1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox nutplay;
+        private System.Windows.Forms.PictureBox ongtren2;
+        private System.Windows.Forms.PictureBox ongduoi2;
+        private System.Windows.Forms.PictureBox pb_Title;
+        private System.Windows.Forms.PictureBox pb_GameOver;
+        private System.Windows.Forms.Label lbldiem;
+
+    }
+}
+
---
+++ FlappyBird_Edited/FlappyBird/ver3/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Drawing.Text;
+
+namespace ver3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int Y_OngTren1, Y_OngTren2;
+
+        int Y_OngDuoi1, Y_OngDuoi2;
+
+
+        int DoLech = 125;
+
+        int X_CapOng1, X_CapOng2;
+
+        int Diem = 0;
+
+        Bitmap bird_1 = new Bitmap(Properties.Resources.redbird_downflap),
+            bird_2 = new Bitmap(Properties.Resources.redbird_midflap),
+            bird_3 = new Bitmap(Properties.Resources.redbird_upflap);
+
+        Bitmap[] array_Bird; 
+        Bitmap birdPicture;
+        Timer bird_Timer = new Timer();
+
+        float X_Bird = 60;
+        float Y_Bird = 200;
+
+        int count = 0;
+
+        private void Bird_Animator()
+        {
+
+            bird_Timer.Interval = 100;
+            bird_Timer.Tick += Bird_Timer_Tick;
+            bird_Timer.Start();
+        }
+
+        
+
+        private Bitmap Draw2D_Bird()
+        {
+            array_Bird = new Bitmap[] { bird_1, bird_2, bird_3 };
+
+            if (count < array_Bird.Length)
+            {
+                birdPicture = array_Bird[count++];
+            }
+            else
+            {
+                count = 0;
+            }
+
+            return birdPicture;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (bird_Timer.Enabled == true)
+            {
+                e.Graphics.DrawImage(Draw2D_Bird(), X_Bird, Y_Bird);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+           
+
+            X_CapOng1 = this.Width + 150;
+            Y_OngTren1 = -150;
+            
+            ongtren1.Location = new Point(X_CapOng1, Y_OngTren1);
+            Y_OngDuoi1 = (250 + Y_OngTren1) + DoLech;
+        
+            ongduoi1.Location = new Point(X_CapOng1, Y_OngDuoi1);
+
+            X_CapOng2 = X_CapOng1 + ongtren1.Width + 250;
+            Y_OngTren2 = -150;
+          
+            ongtren2.Location = new Point(X_CapOng2, Y_OngTren2);
+            Y_OngDuoi2 = (250 + Y_OngTren2) + DoLech;
+           
+            ongduoi2.Location = new Point(X_CapOng2, Y_OngDuoi2);
+
+
+
+            timer1.Interval = 35;
+            //timer2.Interval = 70;
+               
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint |
+            ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer, true);
+
+            X_CapOng1 -= 10;
+            X_CapOng2 -= 10;
+
+            ongtren1.Location = new Point(X_CapOng1, Y_OngTren1);
+            ongduoi1.Location = new Point(X_CapOng1, Y_OngDuoi1);
+            ongtren2.Location = new Point(X_CapOng2, Y_OngTren2);
+            ongduoi2.Location = new Point(X_CapOng2, Y_OngDuoi2);
+
+            if (X_CapOng1 + ongtren1.Width <= 0)
+            {
+                Diem++;
+
+                X_CapOng1 = 400 + ongtren2.Width + 150;
+
+                Random cao = new Random();
+                Y_OngTren1 = cao.Next(-175, -25);
+                Y_OngDuoi1 = (256 + Y_OngTren1) + DoLech;
+                ongtren1.Location = new Point(X_CapOng1, Y_OngTren1);
+                ongduoi1.Location = new Point(X_CapOng1, Y_OngDuoi1);
+            }
+
+            if (X_CapOng2 + ongtren2.Width <= 0)
+            {
+                Diem++;
+                X_CapOng2 = 400 + ongtren1.Width + 150;
+                Random cao = new Random();
+                Y_OngTren2 = cao.Next(-175, -25);
+                Y_OngDuoi2 = (256 + Y_OngTren2) + DoLech;
+                ongtren2.Location = new Point(X_CapOng2, Y_OngTren2);
+                ongduoi2.Location = new Point(X_CapOng2, Y_OngDuoi2);
+            }
+
+            lbldiem.Text = Diem.ToString();
+
+        }
+        int dem = 0;
+        private void nutplay_Click(object sender, EventArgs e)
+        {
+            nutplay.Visible = false;
+            pb_Title.Visible = false;
+
+            dem++;
+            if (dem % 2 != 0)
+            {
+                timer1.Start();
+                //timer2.Start();
+                Bird_Animator();
+            }
+            else
+            {
+                timer1.Stop();
+         
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                Y_Bird -= 50;
+            }
+
+            //if (e.KeyCode == Keys.A)
+            //{
+            //    Bird_Animator();
+            //}
+            //if (e.KeyCode == Keys.D)
+            //{
+            //    bird_Timer.Stop();
+            //}
+
+        }
+
+     
+
+        private void Bird_Timer_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint |
+            ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer, true);
+
+            if (Y_Bird + birdPicture.Height <= this.Height)
+            {
+                Y_Bird += 10;
+                
+                if (X_Bird + birdPicture.Width >= X_CapOng1 && X_Bird + birdPicture.Width <= X_CapOng1 + ongtren1.Width)
+                {
+                    if (Y_Bird <= 250 + Y_OngTren1 || Y_Bird + birdPicture.Height >= Y_OngDuoi1)
+                    {
+                        timer1.Stop();
+                        pb_GameOver.Visible = true;
+
+                        ongduoi1.Visible = false;
+                        ongduoi2.Visible = false;
+                        ongtren1.Visible = false;
+                        ongtren2.Visible = false;
+
+                      
+
+                      
+                    }
+                }
+                if (X_Bird + birdPicture.Width >= X_CapOng2 && X_Bird + birdPicture.Width <= X_CapOng2 + ongtren2.Width)
+                {
+                    if (Y_Bird <= 250 + Y_OngTren2 || Y_Bird + birdPicture.Height >= Y_OngDuoi2)
+                    {
+                        timer1.Stop();
+                        pb_GameOver.Visible = true;
+
+                        ongduoi1.Visible = false;
+                        ongduoi2.Visible = false;
+                        ongtren1.Visible = false;
+                        ongtren2.Visible = false;
+
+                    
+
+                       
+                    }
+                }
+
+            }
+            else
+            {
+                timer1.Stop();
+              
+                bird_Timer.Stop();
+                pb_GameOver.Visible = true;
+
+                ongduoi1.Visible = false;
+                ongduoi2.Visible = false;
+                ongtren1.Visible = false;
+                ongtren2.Visible = false;
+
+                
+              
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbldiem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ongtren1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+
+
+
+
+    }
+}
