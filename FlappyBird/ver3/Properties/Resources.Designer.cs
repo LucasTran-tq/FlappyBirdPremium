ed0c4d19ec30721e6539d60cf223adcb45b99490
PHUONGVU\Nguyen <phuongvu011101@gmail.com>
2020-12-04 07:40:02 +0700

    add

--- FlappyBird/ver3/Properties/Resources.Designer.cs
+++ FlappyBird/ver3/Properties/Resources.Designer.cs
@@ -131,6 +131,16 @@ namespace ver3.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap fb_intro {
+            get {
+                object obj = ResourceManager.GetObject("fb_intro", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap gameover {
             get {
                 object obj = ResourceManager.GetObject("gameover", resourceCulture);
